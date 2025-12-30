3cbefc236eba3abc5d6b9eea302ac5a4ea878765
fszk0720 <fszk.0720@gmail.com>
2025-12-30 09:52:59 +0900

    修正

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const now = new Date();
 
   if (now >= start && now <= end) {
-    location.href = "./transition/transition.html";
+    location.href = "./top/transition/transition.html";
     return;
   }
 
@@ -74,7 +74,7 @@ setTimeout(() => {
     shoji.classList.add("open");
 
     setTimeout(() => {
-      location.href = "./transition/transition.html";
+      location.href = "./top/transition/transition.html";
     }, 1000);
 
   }, 1000); // ← 追加した 1 秒待機
