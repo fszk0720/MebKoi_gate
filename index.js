0c6aea415528e69fb2b59bc7624558321314a5d8
fszk0720 <fszk.0720@gmail.com>
2025-12-30 11:03:38 +0900

    syuusei

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
   // ★ 前回の状態を必ずリセット
   window.effectStarted = false;
 
-  const start = new Date(2025, 11, 30, 10, 52, 0);
+  const start = new Date(2025, 11, 30, 10, 55, 0);
   const end   = new Date(2026, 0, 2, 21, 59, 59);
 
   // ★ GitHub Pages で即リダイレクトされる原因を削除
